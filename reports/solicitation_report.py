d5c454adf9d027f72c0f91dc1296b80b4632c04e
RafaelLeiteFernandes <rafael.fernandesAimec@gmail.com>
2024-07-18 17:43:36 -0300

    alteração de layout relatório

--- reports/solicitation_report.py
+++ reports/solicitation_report.py
@@ -1,84 +1,52 @@
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, Image, PageBreak
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, Image
 import io
 
 def generate_solicitation_pdf(solicitation_data):
     buffer = io.BytesIO()
-    doc = SimpleDocTemplate(buffer, pagesize=A4)
-    
+    doc = SimpleDocTemplate(
+        buffer,
+        pagesize=A4,
+        leftMargin=20,
+        rightMargin=20,
+        topMargin=20,
+        bottomMargin=20
+    )
+
     elements = []
 
-    # Cabeçalho
-    logo_path = "reports/static/logo.png"  # Caminho para o logo
-    try:
-        logo = Image(logo_path, width=50, height=50)
-        elements.append(logo)
-        elements.append(Spacer(1, 12))
-    except IOError:
-        print(f"Warning: Logo at {logo_path} not found, skipping logo.")
-    
-    header_style = ParagraphStyle(
-        name='Header',
-        fontSize=14,
-        leading=16,
+    # Estilos
+    styles = getSampleStyleSheet()
+    title_style = ParagraphStyle(
+        name='TitleStyle',
+        parent=styles['Heading1'],
+        fontSize=16,
         alignment=1,  # Center
-        spaceAfter=10,
+        spaceAfter=12
     )
-    header = Paragraph("Relatório de Solicitação", header_style)
-    elements.append(header)
-
-    # Data
-    date_style = ParagraphStyle(
-        name='Date',
+    subtitle_style = ParagraphStyle(
+        name='SubtitleStyle',
+        parent=styles['Heading2'],
+        fontSize=14,
+        alignment=1,  # Center
+        spaceAfter=12
+    )
+    h2_style = ParagraphStyle(
+        name='H2Style',
+        parent=styles['Heading2'],
+        fontSize=14,
+        spaceAfter=12
+    )
+    normal_style = styles['Normal']
+    normal_bold_style = ParagraphStyle(
+        name='NormalBold',
+        parent=styles['Normal'],
         fontSize=10,
-        leading=12,
-        alignment=2,  # Right
+        spaceAfter=6,
+        fontName='Helvetica-Bold'
     )
-    date = Paragraph(f"Data: {solicitation_data.get('data_solicitacao', 'N/A')}", date_style)
-    elements.append(date)
-    elements.append(Spacer(1, 12))  # Adicionar espaço
-
-    # Dados da Solicitação
-    elements.append(Paragraph(f"Código: {solicitation_data.get('codigo', '')}", getSampleStyleSheet()['Normal']))
-    elements.append(Paragraph(f"Solicitante: {solicitation_data.get('usuario_nome', '')}", getSampleStyleSheet()['Normal']))
-    elements.append(Paragraph(f"Tipo de Retirada: {solicitation_data.get('tipo_retirada_nome', '')}", getSampleStyleSheet()['Normal']))
-    elements.append(Paragraph(f"Destino: {solicitation_data.get('destino_nome', '')}", getSampleStyleSheet()['Normal']))
-    elements.append(Paragraph(f"Observação: {solicitation_data.get('observacao', '')}", getSampleStyleSheet()['Normal']))
-    elements.append(Paragraph(f"Status: {solicitation_data.get('status', '')}", getSampleStyleSheet()['Normal']))
-    elements.append(Spacer(1, 12))  # Adicionar espaço
-
-    # Tabela de Itens da Solicitação
-    table_data = [["Produto", "Quantidade", "Quantidade Separada", "Lote", "Data Fabricação", "Data Validade"]]
-    for item in solicitation_data['items']:
-        table_data.append([
-            item['produto_descricao'],
-            item['quantidade'],
-            item['quantidade_separada'],
-            item.get('lote', '-'),
-            item.get('data_fab', '-'),
-            item.get('data_vlt', '-')
-        ])
-
-    table_style = TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
-        ('GRID', (0, 0), (-1, -1), 1, colors.black),
-    ])
-
-    table = Table(table_data, style=table_style)
-    elements.append(table)
-
-    # Verificar se a tabela cabe na página, senão adicionar uma quebra de página
-    if len(table_data) > 10:  # Ajustar esse valor conforme necessário
-        elements.append(PageBreak())
-
-    # Rodapé
     footer_style = ParagraphStyle(
         name='Footer',
         fontSize=10,
@@ -86,8 +54,80 @@ def generate_solicitation_pdf(solicitation_data):
         alignment=1,  # Center
         spaceBefore=20,
     )
-    footer = Paragraph("Relatório gerado por [Nome da Empresa]", footer_style)
-    elements.append(footer)
+
+    def add_solicitation_section(elements):
+        # Cabeçalho com logo e título em um quadrado
+        header_data = [
+            [
+                Image("reports/static/werner.png", width=80, height=30),
+                Paragraph("Requisição de Retirada", title_style)
+            ]
+        ]
+        header_table = Table(header_data, colWidths=[60, 440])
+        header_table.setStyle(TableStyle([
+            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE')
+        ]))
+        elements.append(header_table)
+        elements.append(Spacer(1, 12))
+
+        # Dados da Solicitação
+        elements.append(Paragraph("Dados da Solicitação", subtitle_style))
+        
+        data_elements = [
+            Paragraph(f"Código: {solicitation_data.get('codigo', '')}", h2_style),
+            Paragraph(f"Solicitante: {solicitation_data.get('usuario_nome', '')}", normal_style),
+            Paragraph(f"Tipo de Retirada: {solicitation_data.get('tipo_retirada_nome', '')}", normal_style),
+            Paragraph(f"Destino: {solicitation_data.get('destino_nome', '')}", normal_style),
+            Paragraph(f"Observação: {solicitation_data.get('observacao', '')}", normal_style),
+            Paragraph(f"Status: {solicitation_data.get('status', '')}", normal_style),
+        ]
+        
+        data_table = Table([
+            [data_elements[0], data_elements[1]],
+            [data_elements[2], data_elements[3]],
+            [data_elements[4], data_elements[5]]
+        ], colWidths=[250, 250])
+        data_table.setStyle(TableStyle([
+            ('VALIGN', (0, 0), (-1, -1), 'TOP')
+        ]))
+        
+        elements.append(data_table)
+        elements.append(Spacer(1, 12))
+
+        # Tabela para os itens da solicitação
+        elements.append(Paragraph("Lista de Produtos", subtitle_style))
+        elements.append(Spacer(1, 12))
+        table_data = [["Código", "Produto / Descrição", "Quantidade"]]
+
+        for item in solicitation_data['items']:
+            row = [
+                item['codpro'],
+                item['produto_descricao'],
+                item['quantidade']
+            ]
+            table_data.append(row)
+
+        table_style = TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+            ('BACKGROUND', (0, 1), (-1, -1), colors.whitesmoke),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ])
+
+        table = Table(table_data, style=table_style, colWidths=[100, 300, 100])
+        elements.append(table)
+
+        # Espaçamento para a próxima seção
+        elements.append(Spacer(1, 48))
+
+    # Adicionar duas seções duplicadas para a folha A4
+    add_solicitation_section(elements)
+    add_solicitation_section(elements)
+
 
     doc.build(elements)
     buffer.seek(0)
